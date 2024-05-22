d7d80f30aeccfb87cccaed878ac52b3e4b0ac3eb
ProgramKingAbel <abelmorara254@gmail.com>
2024-05-22 07:39:53 +0300

    feat(user service): implement create and get users service actions

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-import { ServiceBroker } from "moleculer";
+// import { ServiceBroker } from "moleculer";
 
-const broker = new ServiceBroker();
+// const broker = new ServiceBroker();
 
-broker.createService({
-    name: 'greeter',
-    actions: {
-        sayHello(ctx) {
-            return `Hello ${ctx.params.name}`
-        }
-    }
-});
+// broker.createService({
+//     name: 'greeter',
+//     actions: {
+//         sayHello(ctx) {
+//             return `Hello ${ctx.params.name}`
+//         }
+//     }
+// });
+
+// async function startApp() {
+//     await broker.start()
+//     const res = await broker.call('greeter.sayHello', { name: 'John' })
+//     console.log(res)
+//     broker.stop()
+// }
+
+// startApp()
+
+import UserService from './services/user.service.js'
 
 async function startApp() {
-    await broker.start()
-    const res = await broker.call('greeter.sayHello', { name: 'John' })
-    console.log(res)
-    broker.stop()
+    await UserService.start();
+
+    try {
+        const newUser = await UserService.call('user.createUser', {
+            username: 'Abel',
+            email: 'abel@gmail.com'
+        });
+
+        console.log('New User Created:', newUser)
+        const users = await UserService.call('user.getUsers');
+        console.log('All users:', users);
+
+    } catch (error) {
+        console.log('Error', error)
+    } finally {
+        await UserService.stop();
+    }
 }
 
-startApp()
+startApp();
